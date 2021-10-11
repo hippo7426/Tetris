ee2bdef2fb4dd24a51800a71146d2baeb7964b9e
SeungJun <hippo7426@gmail.com>
2021-10-11 15:53:59 +0900

    Add Play Board

---
+++ script.js
@@ -0,0 +1,14 @@
+// Default Settings
+const COLS = 10;
+const ROWS = 20;
+const BLOCK_SIZE = 30;
+
+//Canvas DOM & Settings
+const canvas = document.getElementById('board');
+const ctx = canvas.getContext('2d');
+
+ctx.canvas.width=COLS*BLOCK_SIZE;
+ctx.canvas.height=ROWS*BLOCK_SIZE;
+
+ctx.scale(BLOCK_SIZE,BLOCK_SIZE);
+
